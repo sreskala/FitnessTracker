15ac11b71a7134320696ee389c65b7f5901294e3
sreskala <sam.sagas@hotmail.com>
2020-12-17 16:59:49 -0500

    workout data class update

--- FitnessTracker.Data/WorkoutData/Workout.cs
+++ FitnessTracker.Data/WorkoutData/Workout.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace FitnessTracker.Data.WorkoutData
 {
     public class Workout
     {
+        [Key]
         public int WorkoutId { get; set; }
+
+        [Required]
         public string Title { get; set; }
 
+        public Guid OwnerId { get; set; }
+
         //reference other tables here
+
+
     }
 }
