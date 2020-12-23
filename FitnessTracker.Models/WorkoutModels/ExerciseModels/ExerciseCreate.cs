b43be46f25c09742ba3707b0fc1d39bc610335f6
sreskala <sam.sagas@hotmail.com>
2020-12-22 21:58:49 -0500

    Added exercise data model and DTO for using with services and controllers

--- FitnessTracker.Models/WorkoutModels/ExerciseModels/ExerciseCreate.cs
+++ FitnessTracker.Models/WorkoutModels/ExerciseModels/ExerciseCreate.cs
@@ -1,39 +1,29 @@
-﻿using System;
+﻿using FitnessTracker.Data.WorkoutData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FitnessTracker.Data.WorkoutData
+namespace FitnessTracker.Models.WorkoutModels.ExerciseModels
 {
-    public enum WorkoutType { Cardio, Weight};
-    public enum MuscleGroup { Chest = 1, FDeltoid, Biceps, Forearms, Abdominals, Obliques, Quadriceps, Tibialis, Trapezius, RDeltoid, MiddleBack, Triceps, LowerBack, Glutes, Hamstrings, Calf }
-    public class Exercise
+    public class ExerciseCreate
     {
-        [Key]
-        public int ExerciseId { get; set; }
-
         [Required]
-        [MaxLength(100, ErrorMessage = "Name too long.")]
+        [MaxLength(100, ErrorMessage = "Name too long")]
         public string Name { get; set; }
 
         [Required]
         [MaxLength(1000, ErrorMessage = "Too many characters.")]
         public string Description { get; set; }
-
         public int Repetition { get; set; }
         public int Sets { get; set; }
         public int Length { get; set; }
         public WorkoutType Type { get; set; }
-
         public MuscleGroup Muscle { get; set; }
 
-        //Reference to workout
         [Required]
-        [ForeignKey(nameof(Workout))]
         public int WorkoutId { get; set; }
-        public virtual Workout Workout { get; set; }
     }
 }
---
+++ FitnessTracker.Models/WorkoutModels/ExerciseModels/ExerciseDetail.cs
@@ -0,0 +1,22 @@
+﻿using FitnessTracker.Data.WorkoutData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Models.WorkoutModels.ExerciseModels
+{
+    public class ExerciseDetail
+    {
+        public int ExerciseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Repetition { get; set; }
+        public int Sets { get; set; }
+        public int Length { get; set; }
+        public WorkoutType Type { get; set; }
+        public MuscleGroup Muscle { get; set; }
+        public int WorkoutId { get; set; }
+    }
+}
