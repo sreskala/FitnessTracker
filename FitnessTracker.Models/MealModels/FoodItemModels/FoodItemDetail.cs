48d9242b1ca2a7b4431165e9888e6d0551af9a8b
sreskala <sam.sagas@hotmail.com>
2021-01-11 16:46:33 -0500

    styled food items views and meal views

--- FitnessTracker.Models/MealModels/FoodItemModels/FoodItemDetail.cs
+++ FitnessTracker.Models/MealModels/FoodItemModels/FoodItemDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace FitnessTracker.Models.MealModels.FoodItemModels
 {
     public class FoodItemDetail
     {
+        [Display(Name = "Food Item #")]
         public int FoodItemId { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
         public int Calories { get; set; }
+
+        [Display(Name = "Tied to Meal #")]
         public int MealId { get; set; }
     }
 }
--- FitnessTracker.Models/MealModels/FoodItemModels/FoodItemListItem.cs
+++ FitnessTracker.Models/MealModels/FoodItemModels/FoodItemListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace FitnessTracker.Models.MealModels.FoodItemModels
 {
     public class FoodItemListItem
     {
+        [Display(Name = "Food Item #")]
         public int FoodItemId { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Tied to Meal #")]
         public int MealId { get; set; }
     }
 }
